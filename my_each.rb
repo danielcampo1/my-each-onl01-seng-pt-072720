93075ff3c330c3ec1f5ebef82cce3304786c913e
daniel campo <campo_daniel94@yahoo.com>
2020-08-06 21:13:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,22 @@
 require "pry"
 
 
-def my_each (array)
-  i = 0
+# def my_each (array)
+#   i = 0
 
-  while i > 1 && i < 4
-    yield(array[i])
-     i += 1
-      i
-  end
-  array
+#   while i > 1 && i < 4
+#     yield(array[i])
+#     i += 1
+#       i
+#   end
+#   array
+# end
+
+def my_each(collection)
+ count = 0
+ while counter < collection.size
+   yield(collection[count])
+ count += 1
+end
+  collection
 end
